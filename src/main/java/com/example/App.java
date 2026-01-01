50549b49b3e8c7e4b5cbfea9ea66b2576eaacaa1
priyanka-ochaney13 <priyanka.ochaney@gmail.com>
2026-01-01 14:18:03 +0530

    playing with bean

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -14,8 +14,17 @@ public class App
     {
         ApplicationContext context = new AnnotationConfigApplicationContext(com.example.config.Appconfig.class); 
         //ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-        Desktop desktop = context.getBean("desk1", Desktop.class);
-        desktop.compile();
+        Student st = context.getBean(Student.class);
+        st.setAge(20);
+        System.out.println(st.getAge());
+        st.code();
+
+        // Desktop desktop = context.getBean("desk1", Desktop.class);
+        // System.out.println("obj1 created");
+
+        // Desktop desktop2 = context.getBean("desk1", Desktop.class);
+        // System.out.println("obj2 created");
+
         
     }
 }
--- src/main/java/com/example/config/Appconfig.java
+++ src/main/java/com/example/config/Appconfig.java
@@ -1,12 +1,35 @@
 package com.example.config;
 import com.example.Desktop;
+import com.example.Student;
+import com.example.Computer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 
 @Configuration
 public class Appconfig {
     @Bean (name={"com1", "desk1"}) // multiple names for a single bean but now if we want to call this bean by desktop then it will give error 
+    @Scope("prototype")
     public Desktop desktop() {
         return new Desktop();
     }
-}
+    @Bean
+    public Student student(@Autowired Computer computer) {
+        Student student = new Student();
+        student.setComputer(computer); 
+        return student;
+    }
+    // @Bean
+    // public Computer computer() {
+    //     return new Desktop(); 
+    //     // returning Desktop object as Computer type bwecause Desktop implements Computer interface and thus we can return Desktop object as Computer type
+    //     // this is called polymorphism
+    //     //what if we have another class Laptop which also implements Computer interface
+    //     // we can create another bean method which returns Laptop object as Computer type example below
+    //     // public Computer computer2() {
+    //     //     return new Laptop();
+    //     // }
+    // }
+
+} 
